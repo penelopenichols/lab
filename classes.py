81b2d2fcde18a5f3aa38b6f653cfb87de6852c98
penelopenichols <110852027+penelopenichols@users.noreply.github.com>
2022-08-08 14:26:45 -0500

    Added docstrings to class and all methods

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 class Television:
+    """
+    A class representing details for Television objects
+    """
     MIN_CHANNEL = 0     # Minimum TV channel
     MAX_CHANNEL = 3     # Maximum TV channel
 
@@ -6,17 +9,28 @@ class Television:
     MAX_VOLUME = 2      # Maximum TV volume
 
     def __init__(self):
+        """
+        Constructor to create initial state of Television object
+        """
         self.__channel = Television.MIN_CHANNEL
         self.__volume = Television.MIN_VOLUME
         self.__tvOn = False
 
     def power(self):
+        """
+        Method to change bool __tvOn
+        """
         if self.__tvOn:
             self.__tvOn = False
         else:
             self.__tvOn = True
 
     def channel_up(self):
+        """
+        Method to increment int __channel by one if __tvOn is True,
+        and if __channel is less than MAX_CHANNEL.
+        If __channel is equal to MAX_CHANNEL, it is assigned MIN_CHANNEL
+        """
         if self.__tvOn:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
@@ -24,6 +38,11 @@ class Television:
                 self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self):
+        """
+        Method to decrement int __channel by one if __tvOn is True,
+        and if __channel is greater than MIN_CHANNEL.
+        If __channel is equal to MIN_CHANNEL, it is assigned MAX_CHANNEL
+        """
         if self.__tvOn:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -31,14 +50,27 @@ class Television:
                 self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self):
+        """
+        Method to increment __volume by one if __tvOn is True,
+         and if __volume is less than MAX_VOLUME
+        """
         if self.__tvOn:
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self):
+        """
+        Method to decrement __volume by one if __tvOn is True,
+        and if __volume is greater than MIN_VOLUME
+        """
         if self.__tvOn:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
     def __str__(self):
+        """
+        Method to return the Television object's status
+        including __tvOn, __channel, and __volume
+        :return: String containing TV status
+        """
         return f"TV status: Is on = {self.__tvOn}, Channel = {self.__channel}, Volume = {self.__volume}"
